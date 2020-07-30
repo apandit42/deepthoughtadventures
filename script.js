4e3601335215e2f51376283497ed3e691049a1ba
apandit42 <apandit@stanford.edu>
2020-07-30 13:28:50 -0500

    tingman

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let Charizard = {
+  name: 'Charizard',
+  height: 6,
+  weight: 400,
+  level: 100,
+  pwr: 21.21231,
+  fight: () => {
+    console.log("fuck u cont");
+  },
+  baby: {
+    ugly: "ass bitch"
+  }
+};
+
+class Shape {
+  constructor()
+  {
+    
+  }
+  area() {
+
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super();
+    area();
+    this.width = 5;
+    this.height = 5;
+  }
+}
+
+let ass = new Rectangle(5, 5);
