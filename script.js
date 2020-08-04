771b3a61dd30a3ecb739be6af15b3d8f8e28aff7
apandit42 <apandit@stanford.edu>
2020-08-04 23:05:30 +0100

    yeehaw

--- script.js
+++ script.js
@@ -6,31 +6,4 @@ let Charizard = {
   weight: 400,
   level: 100,
   pwr: 21.21231,
-  fight: () => {
-    console.log("fuck u cont");
-  },
-  baby: {
-    ugly: "ass bitch"
-  }
 };
-
-class Shape {
-  constructor()
-  {
-    
-  }
-  area() {
-
-  }
-}
-
-class Rectangle extends Shape {
-  constructor(width, height) {
-    super();
-    area();
-    this.width = 5;
-    this.height = 5;
-  }
-}
-
-let ass = new Rectangle(5, 5);
